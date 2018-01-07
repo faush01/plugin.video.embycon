ed8d32ad57cf6f26ad444aec04477a8eb13203df
im85288 <ianmalcolmmclaughlin@gmail.com>
2018-01-07 14:00:52 +0000

    set user image in windows props for skins

--- resources/lib/downloadutils.py
+++ resources/lib/downloadutils.py
@@ -135,6 +135,10 @@ class DownloadUtils():
             artwork += '&MaxHeight=%s' % height
         return artwork
 
+    def get_user_artwork(self, item_id, item_type):
+        # Load user information set by UserClient
+        return "%s/emby/Users/%s/Images/%s?Format=original" % (self.getServer(), item_id, item_type)
+
     def getUserId(self):
 
         WINDOW = HomeWindow()
@@ -174,10 +178,13 @@ class DownloadUtils():
         log.debug("GETUSER_JSONDATA_02: {0}", result)
 
         userid = ""
+        userImage = ""
         secure = False
         for user in result:
             if (user.get("Name") == unicode(userName, "utf-8")):
                 userid = user.get("Id")
+                if "PrimaryImageTag" in user:
+                    userImage =  self.get_user_artwork(userid, 'Primary')
                 log.debug("Username Found: {0}", user.get("Name"))
                 if (user.get("HasPassword") == True):
                     secure = True
@@ -202,6 +209,7 @@ class DownloadUtils():
         log.debug("userid: {0}", userid)
 
         WINDOW.setProperty("userid", userid)
+        xbmcgui.Window(10000).setProperty("EmbyUserImage", userImage)
 
         return userid
 
