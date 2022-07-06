1e1067b6f7e9896efbb1c75a56995fffdf2e92e8
shaun <shaun@bluebit.com.au>
2022-07-06 11:06:45 +1000

    use portable kodi for testing

--- scripts/copy_embycon.bat
+++ scripts/copy_embycon.bat
@@ -1,15 +1,16 @@
+set KODIPATH=c:\tools\Kodi19
 
-del /F /Q /S %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon
-rmdir /Q /S %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon
+del /F /Q /S %KODIPATH%\addons\plugin.video.embycon
+rmdir /Q /S %KODIPATH%\addons\plugin.video.embycon
 
-xcopy /Y addon.xml %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\
-xcopy /Y default.py %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\
-xcopy /Y fanart.jpg %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\
-xcopy /Y icon.png %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\
-xcopy /Y kodi.png %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\
-xcopy /Y service.py %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\
+xcopy /Y addon.xml %KODIPATH%\addons\plugin.video.embycon\
+xcopy /Y default.py %KODIPATH%\addons\plugin.video.embycon\
+xcopy /Y fanart.jpg %KODIPATH%\addons\plugin.video.embycon\
+xcopy /Y icon.png %KODIPATH%\addons\plugin.video.embycon\
+xcopy /Y kodi.png %KODIPATH%\Kodi\addons\plugin.video.embycon\
+xcopy /Y service.py %KODIPATH%\addons\plugin.video.embycon\
 
-xcopy /E /Y resources %HOMEPATH%\AppData\Roaming\Kodi\addons\plugin.video.embycon\resources\
+xcopy /E /Y resources %KODIPATH%\addons\plugin.video.embycon\resources\
 
-cd "%programfiles%\Kodi"
-kodi.exe
+cd "%KODIPATH%"
+kodi.exe -p
